1f271093f2d02390741e4c67bd7f3de0e7759bec
Ly Nguyen Xuan Thao <>
2024-08-15 19:33:40 +0700

    push push code

--- TMadeCrochet/app/src/main/java/com/tmadecrochet/dev/SplashActivity.java
+++ TMadeCrochet/app/src/main/java/com/tmadecrochet/dev/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.tmadecrochet.dev;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -28,6 +29,6 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
-        }, 3000);
+        }, 9000);
     }
 }
