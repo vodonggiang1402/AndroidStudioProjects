a4dd5c095c4483de7b3ee70bbbcb3f0be1221fcb
Ly Nguyen Xuan Thao <>
2024-08-16 13:01:46 +0700

    push push code push push code

--- TMadeCrochet/app/src/main/java/com/tmadecrochet/dev/SplashActivity.java
+++ TMadeCrochet/app/src/main/java/com/tmadecrochet/dev/SplashActivity.java
@@ -29,6 +29,6 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
-        }, 9000);
+        }, 3000);
     }
 }
