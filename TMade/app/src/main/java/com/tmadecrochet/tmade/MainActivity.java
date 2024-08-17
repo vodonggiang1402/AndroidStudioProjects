7dde97cc4a83af2094153647499d3871786b73bb
Ly Nguyen Xuan Thao <>
2024-08-17 11:48:39 +0700

    push code

--- TMade/app/src/main/java/com/tmadecrochet/tmade/MainActivity.java
+++ TMade/app/src/main/java/com/tmadecrochet/tmade/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
     }
 }
