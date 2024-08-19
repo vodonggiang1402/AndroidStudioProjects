95bd96e1c14accee9a73f7441561b5242837560b
Ly Nguyen Xuan Thao <>
2024-08-19 14:14:34 +0700

    push code

--- TMade/app/src/main/java/com/tmadecrochet/tmade/MainActivity.java
+++ TMade/app/src/main/java/com/tmadecrochet/tmade/MainActivity.java
@@ -1,24 +1,51 @@
 package com.tmadecrochet.tmade;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.tmadecrochet.tmade.fragment.CounterFragment;
+import com.tmadecrochet.tmade.fragment.SettingFragment;
+import com.tmadecrochet.tmade.fragment.SymbolFragment;
+import com.tmadecrochet.tmade.fragment.TutorialFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_symbol);
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+        Fragment selectedFragment = new SymbolFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, selectedFragment).commit();
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener = item -> {
+        int id = item.getItemId();
+        Fragment selectedFragment = null;
+
+        if (id == R.id.bottom_symbol) {
+            selectedFragment =  new SymbolFragment();
+        } else if (id == R.id.bottom_counter) {
+            selectedFragment =  new CounterFragment();
+        } else if (id == R.id.bottom_tutorial) {
+            selectedFragment =  new TutorialFragment();
+        } else if (id == R.id.bottom_setting) {
+            selectedFragment =  new SettingFragment();
+        }
+
+        getSupportFragmentManager ().beginTransaction().replace(R.id.frameLayout, selectedFragment).commit();
+
+        return true;
+    };
 }
---
+++ TMade/app/src/main/java/com/tmadecrochet/tmade/fragment/CounterFragment.java
@@ -0,0 +1,21 @@
+package com.tmadecrochet.tmade.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tmadecrochet.tmade.R;
+
+public class CounterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_counter, container, false);
+    }
+}
